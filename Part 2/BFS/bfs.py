1a8db2244ab10e4b9e4d8bfdd886361c99453cf9
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2018-12-23 15:02:52 +0530

    added code for bfs

---
+++ Part 2/BFS/bfs.py
@@ -0,0 +1,25 @@
+from queue import Queue
+
+def bfs(graph, start):
+    '''
+    Input: A graph in adjacency list format and a starting vertex.
+    Output: Prints vertices in BFS traversal.
+    '''
+
+    # Initializing the explored set with the start vertex
+    # Using set for more efficient retrieval
+    explored = set([start])
+
+    # Initializing the queue with the start vertex
+    q = Queue()
+    q.put(start)
+    print(start)
+    
+    # Exactly like the pseudo-code!
+    while not q.empty():
+        v = q.get()
+        for w in graph[v]:
+            if w not in explored:
+                print(w)
+                explored.add(w)
+                q.put(w)
